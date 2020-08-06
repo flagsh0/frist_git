69ae835751c505679f2d9966982ba32b733cc709
兮其 <rose-bengal@users.noreply.github.com>
2020-08-06 10:05:14 +0800

    修改过的代码

---
+++ src/components/login.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt />
+      </div>
+      <!-- 表单 -->
+      <div class="form_box">
+        <el-form :model="form" :rules="formRules" ref="form" label-width="0">
+          <!-- 用户名 -->
+          <el-form-item prop="username">
+            <el-input v-model="form.username" placeholder="请输入内容" prefix-icon="el-icon-s-custom"></el-input>
+          </el-form-item>
+          <!-- 密码 -->
+          <el-form-item prop="password">
+            <el-input
+              v-model="form.password"
+              placeholder="请输入内容"
+              prefix-icon="el-icon-lock"
+              type="password"
+            ></el-input>
+          </el-form-item>
+          <!-- 按钮 -->
+          <el-form-item class="btns">
+            <el-button type="primary">登录</el-button>
+            <el-button type="info">注册</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {
+      form: {
+        username: "cahaha",
+        password: "123",
+      },
+      formRules: {
+        username: [
+          {
+            required: true,
+            message: "请输入用户名",
+            trigger: "blur",
+          },
+          {
+            min: 3,
+            max: 10,
+            message: "长度在 3 到 10 个字符",
+            trigger: "blur",
+          },
+        ],
+        password: [
+          {
+            required: true,
+            message: "请输入密码",
+            trigger: "blur",
+          },
+          {
+            min: 6,
+            max: 15,
+            message: "长度在 6 到 15 个字符",
+            trigger: "blur",
+          },
+        ],
+      },
+    };
+  },
+};
+</script>
+
+<style lang='less' scoped>
+.login_container {
+  height: 100%;
+  background-color: #2b4b6b;
+}
+
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #ffffff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.avatar_box {
+  width: 130px;
+  height: 130px;
+  border: 1px solid #eee;
+  border-radius: 50%;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #ffffff;
+  padding: 10px;
+  box-shadow: 0 0 10px #ddd;
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background-color: #eee;
+  }
+}
+
+.form_box {
+  width: 100%;
+  padding: 20px;
+  box-sizing: border-box;
+  margin-top: 75px;
+  .btns {
+    display: flex;
+    justify-content: flex-end;
+  }
+}
+</style>
